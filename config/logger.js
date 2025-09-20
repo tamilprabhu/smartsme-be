71a4910af2ae6ecfbd610a779089b23faa3bf6fc
Nandha Kumar Viswanathan <nandhakumarviswanaathan@gmail.com>
2025-09-20 13:31:28 +0530

    Add Winston logging to auth routes and update project metadata

---
+++ config/logger.js
@@ -0,0 +1,17 @@
+// logger.js
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.errors({ stack: true }),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.File({ filename: 'logs/app.log' }),
+        new winston.transports.Console()
+    ]
+});
+
+module.exports = logger;
