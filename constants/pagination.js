3db52cb329c6132ff41775898ec1c18e2f0b1866
Nandha Kumar Viswanathan <nandhakumarviswanaathan@gmail.com>
2026-02-10 21:19:31 +0530

    introduce new pagination limits

--- constants/pagination.js
+++ constants/pagination.js
@@ -1,12 +1,13 @@
 class ItemsPerPage {
+    static ONE = 1;
     static TEN = 10;
     static TWENTY_FIVE = 25;
     static FIFTY = 50;
     static HUNDRED = 100;
-    static MAX = 100;
+    static MAX = 1000;
     
     static getValidValues() {
-        return [this.TEN, this.TWENTY_FIVE, this.FIFTY, this.HUNDRED];
+        return [this.ONE, this.TEN, this.TWENTY_FIVE, this.FIFTY, this.HUNDRED, this.MAX];
     }
     
     static isValid(value) {
