9d335c117ca82efe093ff814db01166e188ae617
Nandha Kumar Viswanathan <nandhakumarviswanaathan@gmail.com>
2026-02-27 20:45:34 +0530

    chore: update pm2 production launch script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,11 @@
 module.exports = {
     apps: [{
         name: "smartsme-dev",
-        script: "npm",
-        args: "run dev",
+        script: "node",
+        args: "bin/www",
         env: {
+            NODE_ENV: "production",
+            PORT: 8080,
             DB_HOST: "172.17.0.2",
             DB_NAME: "smartsme",
             DB_USER: "smartsmeusr",
